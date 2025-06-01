18a54561d799fb319b378a0e5c570af1f02cfc40
Shaowu Tang (唐绍武) <swtang918@gmail.com>
2025-06-01 18:23:57 +0800

    name

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,10 +1,9 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Shaowu Tang\' Blog',
-  author: 'Shaowu Tang',
-  headerTitle: 'Freewheeling Thoughts',
-  description: <span className="italic">"I never looked at basketball as work." -- Kobe Bryant</span>,
-
+  title: 'Next.js Starter Blog',
+  author: 'Tails Azimuth',
+  headerTitle: 'TailwindBlog',
+  description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
